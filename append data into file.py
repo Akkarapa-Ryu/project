a2db6d4b22343701da6375415b8fb6789c65560f
Akkarapa-Ryu <108443747+Akkarapa-Ryu@users.noreply.github.com>
2022-10-31 21:00:26 +0700

    Add files via upload

---
+++ append data into file.py
@@ -0,0 +1,20 @@
+﻿import pandas as pd
+import utf8
+#การ appand เข้าไปในไฟล์
+data = {
+    'id': [int(input())],
+    'fname': [input()],
+    'lname': [input()],
+    'date': [input()],
+    'leave': [input()]
+}
+df = pd.DataFrame(data)
+ 
+# append data frame to CSV file
+df.to_csv('editstudents.csv', mode='a',index=False,header=False)
+
+# print message
+print("Data appended successfully.")
+
+dfile = pd.read_csv(r'H:\DST\Python\Project\editstudents.csv')
+print(dfile)
