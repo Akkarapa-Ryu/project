84b9d34cc5e50f6453425f54b56db2d9df178fa3
Akkarapa-Ryu <108443747+Akkarapa-Ryu@users.noreply.github.com>
2022-11-01 21:11:07 +0700

    Add files via upload

---
+++ append data into file_01-11-65.py
@@ -0,0 +1,36 @@
+﻿import pandas as pd
+import datetime
+
+#การ appand เข้าไปในไฟล์
+#ID = int(input())
+def openFile():
+    dfile = pd.read_csv('editstudents.csv')
+    print(dfile)
+
+x = int(input('OPTION:  ' ))
+while True:
+    if x == 1:
+        openFile()  
+        break
+    else:
+
+        SID = []
+        SID.append(int(input("StudentID: ")))
+
+        data = {
+        'id': SID,
+        'fname': [input("Firstname: ")],
+        'lname': [input("Lastname: ")],
+        'date': [input("Date: ")],
+        'leave': [input("Leave (S=Sick/B=Business/T=Travel/O=Other): ")]
+        }
+        df = pd.DataFrame(data)
+ 
+# append data frame to CSV file
+        df.to_csv('editstudents.csv', mode='a',index=False,header=False)
+
+# print message
+        print("Data appended successfully.")
+        break
+
+
